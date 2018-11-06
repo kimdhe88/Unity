0ec4e8bae3087ff1e68e8d3a200334639e8683dc
kimdh <kimdhe88@gamil.com>
2018-11-06 11:16:51 +0900

    init

---
+++ Project/1_CapsuleShooting/Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamageable {
+
+    public float startingHealth;
+    protected float health;
+    protected bool isDead;
+
+    protected virtual void Start()
+    {
+        health = startingHealth;
+    }
+
+    public void TakeHit(float _damage, RaycastHit _hit)
+    {
+        health -= _damage;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected void Die()
+    {
+        isDead = true;
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ Project/1_CapsuleShooting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody> ();
+
+    }
+	
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 _lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(_lookPoint.x, transform.position.y, _lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    
+}
