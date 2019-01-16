ad38d414732577a9ef81c494359d79d07d473dd3
poorants <kimdhe88@gmail.com>
2019-01-16 17:42:45 +0900

    no message

--- Scripts/installationZSH.sh
+++ Scripts/installationZSH.sh
@@ -3,15 +3,62 @@ true=1
 false=0
 
 init(){
-  isROOT=${false}
+  osName=`uname -s`
+  osVersion=`cat /etc/issue | head -1 | awk '{print $1}'`
+
+  if [ "${osName}" != "Linux" ];then
+    echo "This script works only on Linux OS."
+    return ${false}
+  fi
+
+  case ${osVersion} in
+    CentOS)
+    ;;
+    Ubuntu)
+    ;;
+    *)
+      echo "This is an unsupported OS version. [${osVersion}]"
+      return ${false}
+  esac
+
+  if [ "${USER}" == "root" ];then
+    isRoot=${true}
+  else
+    isRoot=${false}
+    printf "Enter the password for the account for the sudo command.";read passwd
+  fi
+
+  return ${true}
 }
 
 main(){
-  IsInstalled;rstcode=$?
-  echo "zsh install code is : ${rstcode}"
+  init;rstcode=$?
+  if [ ${rstcode} -eq ${false} ]; then
+    return
+  fi
+
+  InstallrationZsh;rstcode=$?
+  if [ ${rstcode} -eq ${false} ]; then
+    return
+  fi
 }
 
-IsInstalled(){
+InstallrationZsh(){
+
+  if [ -x $(command -v zsh) ];then
+    echo "zsh is already installed."
+    return ${true}
+  else
+    case ${osVersion} in
+      CentOS)
+      InstallCentOS
+      ;;
+      Ubuntu)
+      InstallUbuntu
+      ;;
+    esac
+  fi
+
   if [ -x $(command -v zsh) ];then
     return ${true}
   else
@@ -19,44 +66,20 @@ IsInstalled(){
   fi
 }
 
-init
-main
-
-
-testFunc(){
-  printf "Enter the password for sudo privileges. : ";read passwd
-  printf "Enter the password for sudo privileges. : ";read passwd
-
-
-  osName=`uname -s`
-  osVersion=`cat /etc/issue | head -1 | awk '{print $1}'`
-  case ${osName} in
-    Linux)
-    echo "Current OS type is [${osName}]"
-    case ${osVersion} in
-      CentOS)
-      echo "Current OS version is [${osVersion}]"
-      yum
-      ;;
-      Ubuntu)
-      echo "Current OS version is [${osVersion}]"
-      ;;
-
-      *)
-        echo "This is an unsupported OS version. [${osVersion}]"
-      ;;
-      esac
-    ;;
-    *)
-    echo "This is an unsupported OS. [${osName}]"
-    ;;
-  esac
-
-
-
-  sudo -S rpm -Uvh http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
-  yum --enablerepo=WANdisco-git --disablerepo=base,updates install -y git nss curl libcurl
-
-  csch -s `which zsh`
-
+InstallCentOS(){
+  if [ ${isRoot} -eq "${true}" ];then
+    yum install -y zsh
+  else
+    echo ${passwd} | sudo -S yum install -y zsh
+  fi
 }
+
+InstallUbuntu(){
+  if [ ${isRoot} -eq "${true}" ];then
+    apt-get install -y zsh
+  else
+    apt-get ${passwd} | sudo -S yum install -y zsh
+  fi
+}
+
+main
